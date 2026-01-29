69537c0a2c203fd427332f466f1767cd6c5ea1b6
Martyna <jm55612@zut.edu.pl>
2026-01-29 03:30:56 +0100

    scroll

--- public/js/themeToggle.js
+++ public/js/themeToggle.js
@@ -27,3 +27,24 @@ themeToggle.addEventListener('click', () => {
     
     updateIcon(isLight);
 });
+
+
+let lastScrollTop = 0;
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', function() {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+
+    if (window.innerWidth <= 1300) {
+        if (scrollTop > lastScrollTop && scrollTop > 100) {
+            header.classList.add('header-hidden');
+        } else {
+            header.classList.remove('header-hidden');
+        }
+    } else {
+        header.classList.remove('header-hidden');
+    }
+    
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; 
+}, false);
